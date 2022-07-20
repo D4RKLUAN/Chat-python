034e111444a6917d08e36b367fbb7c1649cc56fa
D4RKLUAN <luanddlsln075@gmail.com>
2022-07-20 09:13:32 -0300

    first commit

---
+++ cliente.py
@@ -0,0 +1,18 @@
+from http import client
+import socket
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+cliente.connect(('localhost', 8688))
+
+terminado = False
+print('Digite tt para terminar o chat')
+
+while not terminado:
+    cliente.send(input('Mensagem:').encode('utf-8'))
+    msg = cliente.recv(1024).decode('utf-8')
+    if msg == 'tt':
+        terminado = True
+    else:
+        print(msg)
+cliente.close()
